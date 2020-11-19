54038810007f0bbbaae2c2500a3ad1c6f471e17a
Lucas Metz <73494055+lucasmetz@users.noreply.github.com>
2020-11-19 12:33:36 -0800

    Add project files.

---
+++ game/CharacterCreation.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace game
+{
+    partial class CharacterCreation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbS = new System.Windows.Forms.ComboBox();
+            this.cbA = new System.Windows.Forms.ComboBox();
+            this.cbC = new System.Windows.Forms.ComboBox();
+            this.cbI = new System.Windows.Forms.ComboBox();
+            this.cbL = new System.Windows.Forms.ComboBox();
+            this.lblS = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.lblI = new System.Windows.Forms.Label();
+            this.lblL = new System.Windows.Forms.Label();
+            this.cbChar = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbCharacter = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCharacter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbS
+            // 
+            this.cbS.BackColor = System.Drawing.Color.LawnGreen;
+            this.cbS.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbS.FormattingEnabled = true;
+            this.cbS.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.cbS.Location = new System.Drawing.Point(234, 26);
+            this.cbS.Name = "cbS";
+            this.cbS.Size = new System.Drawing.Size(248, 52);
+            this.cbS.TabIndex = 1;
+            this.cbS.SelectedIndexChanged += new System.EventHandler(this.cbS_SelectedIndexChanged);
+            // 
+            // cbA
+            // 
+            this.cbA.BackColor = System.Drawing.Color.HotPink;
+            this.cbA.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbA.FormattingEnabled = true;
+            this.cbA.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.cbA.Location = new System.Drawing.Point(234, 99);
+            this.cbA.Name = "cbA";
+            this.cbA.Size = new System.Drawing.Size(248, 52);
+            this.cbA.TabIndex = 2;
+            // 
+            // cbC
+            // 
+            this.cbC.BackColor = System.Drawing.Color.LawnGreen;
+            this.cbC.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbC.FormattingEnabled = true;
+            this.cbC.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.cbC.Location = new System.Drawing.Point(234, 173);
+            this.cbC.Name = "cbC";
+            this.cbC.Size = new System.Drawing.Size(248, 52);
+            this.cbC.TabIndex = 3;
+            // 
+            // cbI
+            // 
+            this.cbI.BackColor = System.Drawing.Color.HotPink;
+            this.cbI.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbI.FormattingEnabled = true;
+            this.cbI.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.cbI.Location = new System.Drawing.Point(234, 246);
+            this.cbI.Name = "cbI";
+            this.cbI.Size = new System.Drawing.Size(248, 52);
+            this.cbI.TabIndex = 4;
+            // 
+            // cbL
+            // 
+            this.cbL.BackColor = System.Drawing.Color.LawnGreen;
+            this.cbL.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbL.FormattingEnabled = true;
+            this.cbL.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.cbL.Location = new System.Drawing.Point(234, 324);
+            this.cbL.Name = "cbL";
+            this.cbL.Size = new System.Drawing.Size(248, 52);
+            this.cbL.TabIndex = 5;
+            // 
+            // lblS
+            // 
+            this.lblS.AutoSize = true;
+            this.lblS.BackColor = System.Drawing.Color.HotPink;
+            this.lblS.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblS.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblS.Location = new System.Drawing.Point(12, 26);
+            this.lblS.Name = "lblS";
+            this.lblS.Size = new System.Drawing.Size(164, 46);
+            this.lblS.TabIndex = 6;
+            this.lblS.Text = "Strength";
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.BackColor = System.Drawing.Color.LawnGreen;
+            this.lblA.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblA.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA.Location = new System.Drawing.Point(12, 99);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(129, 46);
+            this.lblA.TabIndex = 7;
+            this.lblA.Text = "Agility";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.BackColor = System.Drawing.Color.HotPink;
+            this.lblC.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblC.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblC.Location = new System.Drawing.Point(12, 173);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(176, 46);
+            this.lblC.TabIndex = 8;
+            this.lblC.Text = "Charisma";
+            // 
+            // lblI
+            // 
+            this.lblI.AutoSize = true;
+            this.lblI.BackColor = System.Drawing.Color.LawnGreen;
+            this.lblI.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblI.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblI.Location = new System.Drawing.Point(12, 246);
+            this.lblI.Name = "lblI";
+            this.lblI.Size = new System.Drawing.Size(79, 46);
+            this.lblI.TabIndex = 9;
+            this.lblI.Text = "Wit";
+            // 
+            // lblL
+            // 
+            this.lblL.AutoSize = true;
+            this.lblL.BackColor = System.Drawing.Color.HotPink;
+            this.lblL.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblL.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblL.Location = new System.Drawing.Point(12, 324);
+            this.lblL.Name = "lblL";
+            this.lblL.Size = new System.Drawing.Size(101, 46);
+            this.lblL.TabIndex = 10;
+            this.lblL.Text = "Luck";
+            // 
+            // cbChar
+            // 
+            this.cbChar.BackColor = System.Drawing.Color.Yellow;
+            this.cbChar.Font = new System.Drawing.Font("Trebuchet MS", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbChar.FormattingEnabled = true;
+            this.cbChar.Items.AddRange(new object[] {
+            "Hustler",
+            "Bruiser",
+            "Assassin",
+            "Cyborg"});
+            this.cbChar.Location = new System.Drawing.Point(648, 929);
+            this.cbChar.Name = "cbChar";
+            this.cbChar.Size = new System.Drawing.Size(890, 74);
+            this.cbChar.TabIndex = 11;
+            this.cbChar.SelectedIndexChanged += new System.EventHandler(this.cbChar_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("London", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(63, 467);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(469, 108);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Continue";
+            // 
+            // pbCharacter
+            // 
+            this.pbCharacter.Location = new System.Drawing.Point(655, 12);
+            this.pbCharacter.Name = "pbCharacter";
+            this.pbCharacter.Size = new System.Drawing.Size(883, 872);
+            this.pbCharacter.TabIndex = 0;
+            this.pbCharacter.TabStop = false;
+            this.pbCharacter.Click += new System.EventHandler(this.pbCharacter_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 8.25F);
+            this.label2.Location = new System.Drawing.Point(527, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 23);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "0";
+            // 
+            // CharacterCreation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1742, 1015);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbChar);
+            this.Controls.Add(this.lblL);
+            this.Controls.Add(this.lblI);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.lblS);
+            this.Controls.Add(this.cbL);
+            this.Controls.Add(this.cbI);
+            this.Controls.Add(this.cbC);
+            this.Controls.Add(this.cbA);
+            this.Controls.Add(this.cbS);
+            this.Controls.Add(this.pbCharacter);
+            this.Name = "CharacterCreation";
+            this.Text = "qq";
+            this.Load += new System.EventHandler(this.CharacterCreation_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCharacter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbCharacter;
+        private System.Windows.Forms.ComboBox cbS;
+        private System.Windows.Forms.ComboBox cbA;
+        private System.Windows.Forms.ComboBox cbC;
+        private System.Windows.Forms.ComboBox cbI;
+        private System.Windows.Forms.ComboBox cbL;
+        private System.Windows.Forms.Label lblS;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.Label lblI;
+        private System.Windows.Forms.Label lblL;
+        private System.Windows.Forms.ComboBox cbChar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ game/gameasafile.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game
+{
+    public partial class CharacterCreation : Form
+    {
+        int str;
+        int agl;
+        int chr;
+        int wit;
+        int luck;
+
+        public CharacterCreation()
+        {
+            InitializeComponent();
+        }
+
+        private void CharacterCreation_Load(object sender, EventArgs e)
+        {
+            pbCharacter.ImageLocation = @"";
+        }
+
+        private void cbS_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cbS.Text == "<Reset>")
+            {
+                str = 0;
+               // Reset();
+                cbS.Text = "Strength";
+            }
+        }
+
+        private void pbCharacter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+       private void cbChar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbChar.Text == "Hustler")
+            {
+                pbCharacter.Image = Properties.Resources.hustler;
+            }
+            if (cbChar.Text == "Bruiser")
+            {
+                pbCharacter.Image = Properties.Resources.bruiser;
+            }
+            if (cbChar.Text == "Assassin")
+            {
+                pbCharacter.Image = Properties.Resources.assassin;
+            }
+            if (cbChar.Text == "Cyborg")
+            {
+                pbCharacter.Image = Properties.Resources.cyborg;
+            }
+                pbCharacter.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
